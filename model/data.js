5e2bf82dde60704ff887dcb40dfbac37fecab705
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2024-02-19 10:02:21 +0100

    vuelta a la version anterior a las pruebas

--- model/data.js
+++ model/data.js
@@ -2,8 +2,8 @@ const { ObjectId} = require('mongodb')
 const {Schema, model}= require('mongoose')
 
 const conversion = new Schema({
-  value1: String,
-  value2: String,
+  value1: Number,
+  value2: Number,
   text1: String,
   text2: String,
   _id: {
